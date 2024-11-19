8c4ff44f1090924553acd71ec784765b58544729
wonged6 <eddie.wong@oocl.com>
2024-11-19 16:20:40 +0800

    feat: add generateStatus function

--- src/main/java/org/marsrover/MarsRover.java
+++ src/main/java/org/marsrover/MarsRover.java
@@ -1,7 +1,33 @@
 package org.marsrover;
 
+enum Direction {
+    North("N"), East("E"), South("S"), West("W");
+
+    private String value;
+
+    Direction(final String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+}
+
+
 public class MarsRover {
+    private Direction direction;
+    private int x;
+    private int y;
+
+    public MarsRover() {
+        this.direction = Direction.North;
+        this.x = 0;
+        this.y = 0;
+    }
+
     public String generateStatus() {
-        return "0:0:N";
+        return String.format("%d:%d:%s", x, y, this.direction.toString());
     }
 }
