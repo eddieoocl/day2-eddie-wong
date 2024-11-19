7ff84bcfa53fc580ba6dcd5a1aa0343f69ac63ab
wonged6 <eddie.wong@oocl.com>
2024-11-19 16:30:20 +0800

    test: add 4 test cases for turning right

--- src/test/java/org/marsrover/MarsRoverTest.java
+++ src/test/java/org/marsrover/MarsRoverTest.java
@@ -73,4 +73,60 @@ public class MarsRoverTest {
         // Then
         assertEquals("0:0:N", status);
     }
+
+    @Test
+    public void should_face_east_when_direction_north_and_turn_right() {
+        // Given
+        MarsRover rover = new MarsRover(Direction.North);
+        Rotation rotation = Rotation.Right;
+
+        // When
+        rover.rotate(rotation);
+        String status = rover.generateStatus();
+
+        // Then
+        assertEquals("0:0:E", status);
+    }
+
+    @Test
+    public void should_face_north_when_direction_west_and_turn_right() {
+        // Given
+        MarsRover rover = new MarsRover(Direction.West);
+        Rotation rotation = Rotation.Right;
+
+        // When
+        rover.rotate(rotation);
+        String status = rover.generateStatus();
+
+        // Then
+        assertEquals("0:0:N", status);
+    }
+
+    @Test
+    public void should_face_west_when_direction_south_and_turn_right() {
+        // Given
+        MarsRover rover = new MarsRover(Direction.South);
+        Rotation rotation = Rotation.Right;
+
+        // When
+        rover.rotate(rotation);
+        String status = rover.generateStatus();
+
+        // Then
+        assertEquals("0:0:W", status);
+    }
+
+    @Test
+    public void should_face_south_when_direction_east_and_turn_right() {
+        // Given
+        MarsRover rover = new MarsRover(Direction.East);
+        Rotation rotation = Rotation.Right;
+
+        // When
+        rover.rotate(rotation);
+        String status = rover.generateStatus();
+
+        // Then
+        assertEquals("0:0:S", status);
+    }
 }
